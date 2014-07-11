68504dedfb621511ee09a08876fc8448b984aafc
Adam George <github@adamgeorge.com>
2014-07-11 14:10:31 +1000

    Don't need dedicated pages for these models

--- vendor/extensions/products/db/seeds.rb
+++ vendor/extensions/products/db/seeds.rb
@@ -15,34 +15,15 @@ Refinery::I18n.frontend_locales.each do |lang|
     )
   end if defined?(Refinery::User)
 
-  Refinery::Page.where(link_url: (url = "/products")).first_or_create!(
-    title: 'Products',
-    deletable: false,
-    menu_match: "^#{url}(\/|\/.+?|)$"
-  ) do |page|
-    Refinery::Pages.default_parts.each_with_index do |part, index|
-      page.parts.build title: part, body: nil, position: index
-    end
-  end if defined?(Refinery::Page)
-end
-Refinery::I18n.frontend_locales.each do |lang|
-  I18n.locale = lang
-
-  Refinery::User.find_each do |user|
-    user.plugins.where(name: 'refinerycms-products').first_or_create!(
-      position: (user.plugins.maximum(:position) || -1) +1
-    )
-  end if defined?(Refinery::User)
-
-  Refinery::Page.where(link_url: (url = "/products/categories")).first_or_create!(
-    title: 'Categories',
-    deletable: false,
-    menu_match: "^#{url}(\/|\/.+?|)$"
-  ) do |page|
-    Refinery::Pages.default_parts.each_with_index do |part, index|
-      page.parts.build title: part, body: nil, position: index
-    end
-  end if defined?(Refinery::Page)
+  #Refinery::Page.where(link_url: (url = "/products")).first_or_create!(
+  #  title: 'Products',
+  #  deletable: false,
+  #  menu_match: "^#{url}(\/|\/.+?|)$"
+  #) do |page|
+  #  Refinery::Pages.default_parts.each_with_index do |part, index|
+  #    page.parts.build title: part, body: nil, position: index
+  #  end
+  #end if defined?(Refinery::Page)
 end
 
 Refinery::I18n.frontend_locales.each do |lang|
@@ -54,13 +35,33 @@ Refinery::I18n.frontend_locales.each do |lang|
     )
   end if defined?(Refinery::User)
 
-  Refinery::Page.where(link_url: (url = "/products/vehicles")).first_or_create!(
-    title: 'Vehicles',
-    deletable: false,
-    menu_match: "^#{url}(\/|\/.+?|)$"
-  ) do |page|
-    Refinery::Pages.default_parts.each_with_index do |part, index|
-      page.parts.build title: part, body: nil, position: index
-    end
-  end if defined?(Refinery::Page)
+  #Refinery::Page.where(link_url: (url = "/products/categories")).first_or_create!(
+  #  title: 'Categories',
+  #  deletable: false,
+  #  menu_match: "^#{url}(\/|\/.+?|)$"
+  #) do |page|
+  #  Refinery::Pages.default_parts.each_with_index do |part, index|
+  #    page.parts.build title: part, body: nil, position: index
+  #  end
+  #end if defined?(Refinery::Page)
+end
+
+Refinery::I18n.frontend_locales.each do |lang|
+  I18n.locale = lang
+
+  Refinery::User.find_each do |user|
+    user.plugins.where(name: 'refinerycms-products').first_or_create!(
+      position: (user.plugins.maximum(:position) || -1) +1
+    )
+  end if defined?(Refinery::User)
+
+  #Refinery::Page.where(link_url: (url = "/products/vehicles")).first_or_create!(
+  #  title: 'Vehicles',
+  #  deletable: false,
+  #  menu_match: "^#{url}(\/|\/.+?|)$"
+  #) do |page|
+  #  Refinery::Pages.default_parts.each_with_index do |part, index|
+  #    page.parts.build title: part, body: nil, position: index
+  #  end
+  #end if defined?(Refinery::Page)
 end
