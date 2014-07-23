e445177decb88c6a23fba8a9e2b3e02bc0570cf7
Adam George <github@adamgeorge.com>
2014-07-23 16:38:09 +1000

    Don't re-seed these tables

--- vendor/extensions/products/db/seeds.rb
+++ vendor/extensions/products/db/seeds.rb
@@ -3,8 +3,8 @@
 #load 'vendor/extensions/products/db/seeds/01_users.rb'
 #load 'vendor/extensions/products/db/seeds/02_vehicles.rb'
 #load 'vendor/extensions/products/db/seeds/03_categories.rb'
-load 'vendor/extensions/products/db/seeds/04_accessory_products.rb'
-load 'vendor/extensions/products/db/seeds/05_suspension_products.rb'
+#load 'vendor/extensions/products/db/seeds/04_accessory_products.rb'
+#load 'vendor/extensions/products/db/seeds/05_suspension_products.rb'
 
 Refinery::I18n.frontend_locales.each do |lang|
   I18n.locale = lang
