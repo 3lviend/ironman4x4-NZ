808b9d1bb8c494714c9118eff50ff76b8f90fb9b
Adam George <github@adamgeorge.com>
2014-07-28 07:35:32 +1000

    Override params for post

--- vendor/extensions/posts/app/controllers/refinery/posts/admin/posts_controller.rb
+++ vendor/extensions/posts/app/controllers/refinery/posts/admin/posts_controller.rb
@@ -7,6 +7,12 @@ module Refinery
                 :title_attribute => 'type',
                 :xhr_paging => true
 
+      protected
+        def post_params
+          params.require(:post).permit(:type, :title,
+            :teaser, :body, :published_at, :expire_at, :draft,
+            :locale)
+        end
       end
     end
   end
