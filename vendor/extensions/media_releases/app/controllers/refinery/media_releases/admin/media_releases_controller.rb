a247b13fa03aca46be6b040c911720d8cbeb41d7
Adam George <github@adamgeorge.com>
2014-08-08 09:43:09 +1000

    Updating media_releases with image/resource pickers, permit params, etc

--- vendor/extensions/media_releases/app/controllers/refinery/media_releases/admin/media_releases_controller.rb
+++ vendor/extensions/media_releases/app/controllers/refinery/media_releases/admin/media_releases_controller.rb
@@ -6,6 +6,14 @@ module Refinery
         crudify :'refinery/media_releases/media_release',
                 :xhr_paging => true
 
+      protected
+        def media_releases_params
+          params.require(:media_release).permit(:title, :description, :category,
+            :publication, :published_at, :posted_at, :draft, :locale,
+            :thumbnail_image_id, :priced_resource_id, :unpriced_resource_id,
+            :position)
+        end
+
       end
     end
   end
