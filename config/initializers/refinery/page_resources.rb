781cdc411ad356e8dc1ab2cb4e0ab27c21934787
Adam George <github@adamgeorge.com>
2014-08-16 08:57:14 +1000

    Adding page-resources to products too

--- config/initializers/refinery/page_resources.rb
+++ config/initializers/refinery/page_resources.rb
@@ -1,5 +1,8 @@
 Refinery::PageResources.configure do |config|
   # config.captions = false
-  # config.enable_for = [{:model=>"Refinery::Page", :tab=>"Refinery::Pages::Tab"}]
+  config.enable_for = [
+    {:model=>"Refinery::Page", :tab=>"Refinery::Pages::Tab"},
+    {:model=>"Refinery::Ironman::Product", :tab=>"Refinery::Products::Tab"}
+  ]
   # config.wysiwyg = true
 end
