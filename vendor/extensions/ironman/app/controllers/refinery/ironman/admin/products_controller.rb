2ca6de1c7ede7fe595eed3112f9cc762e19d908e
Adam George <github@adamgeorge.com>
2014-08-26 17:43:25 +1000

    Saving vehicles for products, and ensuring params are permitted

--- vendor/extensions/ironman/app/controllers/refinery/ironman/admin/products_controller.rb
+++ vendor/extensions/ironman/app/controllers/refinery/ironman/admin/products_controller.rb
@@ -12,8 +12,9 @@ module Refinery
         def product_params
           p = params.require(:product).permit(
             :product_no, :name, :description, :notes, :quantity_required,
-            :price, :locale, :short_description,
-            :thumbnail_image_id, :fitting_instructions_resource_id, :warranty,
+            :price, :locale, :short_description, {:category_ids => []},
+            {:vehicle_ids => []}, :thumbnail_image_id,
+            :fitting_instructions_resource_id, :warranty,
             images_attributes: [:id], resources_attributes: [:id],
             specifications_attributes: [:id, :_destroy, :title, :value])
 
