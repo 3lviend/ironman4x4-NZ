b3b11dc97243d63eff114ea52e2a099d678da6b9
Adam George <github@adamgeorge.com>
2014-08-27 17:14:18 +1000

    Adding basic ordering for products list

--- vendor/extensions/ironman/app/controllers/refinery/ironman/admin/products_controller.rb
+++ vendor/extensions/ironman/app/controllers/refinery/ironman/admin/products_controller.rb
@@ -7,11 +7,28 @@ module Refinery
                 :title_attribute => 'product_no',
                 :xhr_paging => true,
                 :sortable => false,
-                :per_page => 50,
+                :per_page => 100,
                 :include => [:categories, :vehicles, :images],
                 :order => 'refinery_ironman_categories.name, refinery_ironman_products.product_no'
 
       protected
+        def find_all_products(conditions = '')
+          direction = (params[:direction] == 'desc' ? 'desc' : 'asc')
+
+          orders = {
+            :product_no => 'refinery_ironman_products.product_no ' + direction,
+            :title => 'refinery_ironman_products.name ' + direction,
+            :category => 'refinery_ironman_categories.name ' + direction,
+            :price => 'refinery_ironman_products.price ' + direction
+          }.with_indifferent_access
+
+          order = orders[params[:order]] ||= 'refinery_ironman_categories.name, refinery_ironman_products.product_no'
+
+          @products = find_product_scope
+                              .where(conditions)
+                              .order(order)
+        end
+
         def product_params
           p = params.require(:product).permit(
             :product_no, :name, :description, :notes, :quantity_required,
