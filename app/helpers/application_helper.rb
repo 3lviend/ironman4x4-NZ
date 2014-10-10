650e0b5d2a07788a2769eb7bd27191bfd9fc79fc
Adam George <github@adamgeorge.com>
2014-10-10 14:36:10 +1100

    Fixing links on stockists popover

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -25,4 +25,10 @@ module ApplicationHelper
         flash_type.to_s
     end
   end
+
+  def phone_number_link(text, options)
+    sets_of_numbers = text.scan(/[0-9]+/)
+    number = "+1-#{sets_of_numbers.join('-')}"
+    link_to text, "tel:#{number}", options
+  end
 end
