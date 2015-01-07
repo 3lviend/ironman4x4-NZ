0952ce69787cfb6e11c790bb1c45d1b4aff0db18
Adam George <github@adamgeorge.com>
2015-01-07 16:05:51 +1100

    Might as well leave caching on in development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -11,7 +11,7 @@ Rails.application.configure do
 
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
-  config.action_controller.perform_caching = false
+  config.action_controller.perform_caching = true
 
   if config.respond_to?(:action_mailer)
     # for use with mailcatcher
