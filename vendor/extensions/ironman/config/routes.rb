c5318e1a7a959fdcfd444a6bb7db40cf702652e6
Adam George <github@adamgeorge.com>
2015-02-20 16:59:39 +1100

    Use 302 status for the redirect

--- vendor/extensions/ironman/config/routes.rb
+++ vendor/extensions/ironman/config/routes.rb
@@ -130,5 +130,5 @@ Refinery::Core::Engine.routes.draw do
   end
 
   # TODO: remove this. Temporarily redirect 404s to root page
-  # get '*path', to: redirect('/') unless Rails.env.development?
+  get '*path', to: redirect('/', status:302) unless Rails.env.development?
 end
