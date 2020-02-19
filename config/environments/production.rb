9be31b2b9bb25a1c961d8406dfa34c186b1f72a0
Derek Kaijser <derek@reformedridez.com>
2020-02-20 09:28:09 +1300

    Add time_zone config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  
+  config.time_zone = "Pacific/Auckland"
 
   # Code is not reloaded between requests.
   config.cache_classes = true
